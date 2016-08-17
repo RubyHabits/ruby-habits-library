3cf86229602dfc56506fafba7934efec61004f4a
alicetragedy <girl@alicetragedy.org>
2016-08-17 21:39:07 +0200

    create static pages controller and views with github sign in link, change routes accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #get 'static_pages/landing'
+
+  root to: "static_pages#landing"
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
+
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
