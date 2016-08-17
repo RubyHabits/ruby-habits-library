7e0e0c40bd102924923d5bfab75502bc157a6e02
alicetragedy <girl@alicetragedy.org>
2016-08-17 21:53:15 +0200

    configure environment variables for github

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,13 @@ module RubyHabitsLibrary
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # preload tokens in application.yml to local ENV
+    config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+    config.merge! config.fetch(Rails.env, {})
+    config.each do |key, value|
+      ENV[key] = value.to_s unless value.kind_of? Hash
+    end
+
   end
 end
